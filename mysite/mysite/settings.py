0bb6c3f98783ef958b93ac87c06cccc03e485742
a1chan0210 <a1chan@ucsd.edu>
2022-04-11 04:38:05 -0700

    debug

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!x2nzmsu-_ihnvez&mxz@v580y=@8@hfirx_gqncblgx1$=fh9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['164.92.114.154','localhost']
 
