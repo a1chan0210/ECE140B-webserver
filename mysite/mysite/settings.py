cb62af02ad80ea4806f16f96096e47c5854bb9a7
a1chan0210 <a1chan@ucsd.edu>
2022-04-11 04:35:47 -0700

    database

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -74,13 +74,25 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
-DATABASES = {
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'webserver',
+        'USER': 'ant',
+        'PASSWORD': 'ECE140B',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
+
 
 
 # Password validation
